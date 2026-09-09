7b4843686165a79579e51d46346e03a76ca74878
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2026-09-09 14:21:50 -0500

    Add save_songs() function and test song count display

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import csv
 
-#Function attempts to open and read a CSV file. If it exists, converts each row into a dict and adds it to a list. If it doesn't exist yet, catch the error and return an empty list instead. Function will return a list of dicts (empty or with data)
-def cargar_canciones(nombre_archivo):
-    canciones = []
+#This function open and read a CSV file, and turn each row into a dict and adds it to a list (empty or with data)
+def load_songs(file_name):
+    songs = []
     try:
-        with open(nombre_archivo, mode="r") as archivo:
-            lector = csv.DictReader(archivo)
-            for fila in lector:
-                canciones.append(fila)
+        with open(file_name, mode="r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                songs.append(row)
     except FileNotFoundError:
         pass
-    return canciones
+    return songs
 
-resultado = cargar_canciones("canciones.csv") #Works
+list_of_songs = load_songs("songs.csv")
+print(f"You have {len(list_of_songs)} songs saved")
+
+#This function will add songs to the csv file
+def save_songs(song_list, file_name):
+    fields = ['name', 'artist', 'mood']
+    with open(file_name, mode="w") as file:
+        writer = csv.DictWriter(file, fieldnames=fields)
+        writer.writeheader()
+        for item in song_list :
+            writer.writerow(item)
+
+test_songs=[{"name": "Bohemian Rhapsody", "artist": "Queen", "mood": "Epic"},{"name": "Gasolina", "artist": "Daddy Yankee", "mood": "Happy"}]
+save_songs(test_songs, "songs.csv")
