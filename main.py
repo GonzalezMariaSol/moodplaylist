2845e58f682dcf9ea80c54493ed2079e8a922c7f
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2026-09-17 10:58:13 -0500

    Add function main loop, refactor choose_mood with reusable get_valid_number and explanatory comments

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import csv
 MOODS = ["Happy", "Sad", "Emotional", "Chill", "Party", "Workout"]
 
 
-def show_menu (songs_list):
-    print("Welcome to MoodPlaylist 🎵")
+# $This fuction will show a type of menu based if in the list of songs have or have not songs in it
+
+def show_menu(songs_list):
+    print("\n Welcome to MoodPlaylist 🎵")
     print("---------------------------")
     if len(songs_list) > 0:
-        print("1. Add song")
-        print("2. Generate playlist")
-        print("3. View songs")
-        print("4. Exit")
+        options = ["Add song", "Generate playlist", "View songs", "Exit"]
     else:
-        print("1. Add song")
-        print("2. Exit")
+        options = ["Add song", "Exit"]
+
+    for i, option in enumerate(options, start=1):
+        print(f"{i}. {option}")
+
+    return options  # ~return the options/menu based on how many or if any songs are in the list
 
 
 
@@ -47,37 +50,42 @@ def save_songs (list_of_songs, file_name): # ~ save songs will need the list of 
 # $ This function collects the info from user to add a song to the csv file
 
 def add_song (songs_list):
-    song_name = input("Write Song Name:")
-    song_artist = input("Write Song Artist:")
+    song_name = input("\n Write Song Name:")
+    song_artist = input("\n Write Song Artist:")
     song_mood = choose_mood()
     new_song = {"name": song_name, "artist": song_artist, "mood": song_mood}
     songs_list.append(new_song)
 
 
 
-# $ This function asks the user to choose a mood for the song they just added, and keeps asking again if the input isn't a valid option
+# $ This function only evaluate if the user answer is valid or not 
 
-def choose_mood () : 
-    print("Songs Mood:")
-    for i, mood in enumerate(MOODS, start=1): # ~ for each position and value in MOODS (starting position in 1)
-        print(f"{i}. {mood}")
-    prompt_message = "Choose a mood for your song: " # ~ The question that we gonna ask at first to users
-
-    while True: # ~ do an infinite loop that
-        user_mood = input(prompt_message) # ~ ask users to chose a number
+def get_valid_number(max_value, prompt_message):
+    while True: # ~ We start a indless loop (will be repeating forever unless we stop it using a return/break)
+        user_input = input(prompt_message) # ~ we store user answer
         try:
-            number_mood = int(user_mood) # ~ try to save users answer as a number
-        except ValueError: # ~ unless that the answer is anything but a number
-            prompt_message = "Please enter a number, not a text: "  # ~ update the message for next loop
-            continue # ~ we ask again
+            number = int(user_input) # ~ we try to turn that answer into a number (if the user decide to type a "hello" will fall into the except)
+        except ValueError:
+            prompt_message = "\n Please enter a number, not text: " # ~ show error msge
+            continue # ~ and start all over from the user_input ⟳
+        # ~ once the user actually place a valid answer
+        if number < 1 or number > max_value: # ~ if the anser if lower than 1 or greater than max_value (max_value is basically the moods list length)
+            prompt_message = f"\n Please choose a number between 1 and {max_value}: " # ~ show error msge
+            continue # ~ and start all over from user_input ⟳
 
-        if number_mood < 1 or number_mood > len(MOODS): # ~ if the number doesnt fit between 1 to 6
-            prompt_message = "Please choose a number between 1 and 6: "  # ~ update the message for next loop
-            continue # ~ we ask again 
+        return number  # ~when the user place a valid answer will finish the loop, returning the number that it placed
 
-        break # ~we cut the loop once we have a valid answer (a int from 1 to 6)
 
-    return MOODS[number_mood - 1] # ~ our function will return the mood that is under that number (the number has a -1 for position purposes)    
+
+# $ This function will ask a user to choose a mood based on its number
+
+def choose_mood():
+    print("\n SONG MOOD:")
+    for i, mood in enumerate(MOODS, start=1): # ~ for each position and mood in moods, starting from position 1
+        print(f"{i}. {mood}")
+    
+    number_mood = get_valid_number(len(MOODS), "\n Choose a mood for your song: ") # ~ we save number chosen from user 
+    return MOODS[number_mood - 1] # ~ and we return the mood that belongs to that number (we use - 1 for positions purposes)
 
 
 
@@ -91,21 +99,40 @@ def view_songs (songs_list):
 
 # $ This function will generate a playlist based on the users mood
 
-def generate_playlist (songs_list):
-    mood_user_input = choose_mood() # ~ we reuse the funcion choose_mood to ask again which mood wants to choose and get a right answer without mistakes
+def generate_playlist(songs_list):
+    mood_user_input = choose_mood()
     songs_mood = []
-    for song in songs_list: 
-        if song["mood"] == mood_user_input: # ~ if the song mood is same as the mood that the user choosed
-            songs_mood.append(song) # ~ we add it to the list of songs that are the same mood
-            print(f"{song['name']} - {song['artist']}") # ~ and we show to the user the songs that belong the same mood, mood that it chose
+    for song in songs_list:
+        if song["mood"] == mood_user_input:
+            songs_mood.append(song)
+
+    print(f"\n{mood_user_input.upper()} PLAYLIST")
+    print("---------------------------")
+    for i, song in enumerate(songs_mood, start=1):
+        print(f"{i}. {song['name']} / {song['artist']}")
 
 
 
-list_of_songs = load_songs("songs.csv") #~create a variable of list of songs that will save the dictionary created in it
-print(f"You have {len(list_of_songs)} songs saved")
+# $ This function is the main lopp that will show a menu, will catch users answer and will run the rigth action based on user picks (add song, generate playlist, view songs, or exit) the loop ends only when user choose exit
 
-add_song(list_of_songs) #! D
-save_songs(list_of_songs, "songs.csv") #! D
-view_songs(list_of_songs) #! D
-generate_playlist(list_of_songs) #! D
-show_menu(list_of_songs)
+def main(songs_list, file_name):
+    while True:  # ~ We start a indless loop asking for a choice until the user exits
+        options = show_menu(songs_list) # ~ we save in options the type of menu that is avaible for the amount of songs that are loaded (empty = menu with 2 options, songs avaible = menu with 4 options)
+        choice_number = get_valid_number(len(options), "\n Choose an option: ") # ~ user have to chose a numb avaible from the menu (len options is in charge to show whats the limit number, either the 2 menu or the 4 menu), and we use get valid number because will let us insist in get a rigth answer till the user answer with a right number avaible
+        chosen_option = options[choice_number - 1] # ~ we turn the number that user chose to a text option from the menu (again using a - 1 for positions purposes, if user chose 1 it really meant it chose the option in position 0)
+
+        if chosen_option == "Add song":
+            add_song(songs_list)  # ~ we call add_song, which will ask the user what song they want to add
+            save_songs(songs_list, file_name)  # ~ we call save_songs so the song the user just added gets saved in the CSV. Otherwise it will be lost as soon as the console restarts
+        elif chosen_option == "Generate playlist":
+            generate_playlist(songs_list)
+        elif chosen_option == "View songs":
+            view_songs(songs_list)
+        elif chosen_option == "Exit":
+            print("Goodbye! 🎵")
+            break  # ~ this is what actually stops the while True loop
+
+
+
+list_of_songs = load_songs("songs.csv")
+main(list_of_songs, "songs.csv")
